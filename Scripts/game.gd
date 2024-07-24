d643328259830a87c075d59e0efb2e1bc6ae7740
philip22203 <163483410+philip22203@users.noreply.github.com>
2024-07-24 21:38:29 +1200

    save changes

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+@onready var player_spawn_pos = $PlayerSpawnPos
+
 var player = null
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	assert(player!=null)
+	player.global_position = player_spawn_pos.global_position
 
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
