1046282151f19af9d55e009a62ac886303fd8632
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-05 09:54:28 +1200

    Fixed major problems
    Fixed restarted and art

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -13,4 +13,4 @@ func _process(delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree() .quit()
 	elif Input.is_action_just_pressed("reset"):
-		get_tree() .reload_current_sscene()
+		get_tree() .reload_current_scene()
