f6a1590f5c67661281dc5b1739d4fd6819bbdd53
philip22203 <163483410+philip22203@users.noreply.github.com>
2024-09-04 20:05:52 +1200

    Fixed enemies and added highscore

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
 signal laser_shot(laser_scene, location)
+signal killed
 
 @export var speed = 300
 
@@ -22,6 +23,13 @@ func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 	velocity = direction * speed
 	move_and_slide()
+	
+	global_position = global_position.clamp(Vector2.ZERO, get_viewport_rect().size)
 
 func shoot():
 	laser_shot.emit(laser_scene, muzzle.global_position)
+
+
+func die():
+	killed.emit()
+	queue_free()
